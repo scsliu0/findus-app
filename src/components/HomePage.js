53e7a87f33f285e9bbd091bf0f3331ffe9ca4feb
Shane Liu <shane1997liu@gmail.com>
2017-10-29 23:18:00 -0400

    Folder Structure and Package.json

---
+++ src/components/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+    render(){
+        return <p>HomePage</p>
+    }
+}
+
+export default HomePage;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import HomePage from './components/HomePage';
+
+render(<HomePage/>, document.querySelector('#main'));
