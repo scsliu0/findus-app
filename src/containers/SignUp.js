db8a3de7077cdccde5fb427bd938a206bf1f434e
Shane Liu <shane1997liu@gmail.com>
2017-11-25 17:25:02 -0500

    Updated SignUp page

--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -1,11 +1,73 @@
 import React from 'react';
+import {grey400} from 'material-ui/styles/colors';
+import Divider from 'material-ui/Divider';
+import RaisedButton from 'material-ui/RaisedButton';
+import MenuItem from 'material-ui/MenuItem';
+import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import PageBase from '../components/PageBase';
 
 class SignUp extends React.Component {
     render(){
+        const styles = {
+            toggleDiv: {
+              maxWidth: 300,
+              marginTop: 40,
+              marginBottom: 5
+            },
+            toggleLabel: {
+              color: grey400,
+              fontWeight: 100
+            },
+            buttons: {
+              marginTop: 30,
+              float: 'right'
+            },
+            saveButton: {
+              marginLeft: 5
+            }
+        };
+
         return(
-            <div className="ProfilePage">
-                SignUp
-            </div>
+            <PageBase title="Sign Up">
+                <div className="SignUp">
+                    <form>
+                        <TextField
+                        hintText="Name"
+                        floatingLabelText="Name"
+                        fullWidth={true}
+                        />
+
+                        <TextField
+                        hintText="E-Mail"
+                        floatingLabelText="E-Mail"
+                        fullWidth={true}
+                        />
+
+                        <TextField
+                          hintText="Password"
+                          floatingLabelText="Password"
+                          fullWidth={true}
+                          type="password"
+                        />
+
+                        <TextField
+                        hintText="Interests"
+                        floatingLabelText="Interests(seperated by a comma)"
+                        fullWidth={true}
+                        />
+
+                        <div style={styles.buttons}>
+                            <RaisedButton label="Cancel"/>
+
+                            <RaisedButton label="Save"
+                                        style={styles.saveButton}
+                                        type="submit"
+                                        primary={true}/>
+                        </div>
+                    </form>
+                </div>
+            </PageBase>
         )
     }
 }
